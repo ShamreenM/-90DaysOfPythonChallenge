edf8513201e2aa2684ea635977e7357ea014275c
Shamreen-M <shamreen.khadija@gmail.com>
2026-04-14 20:53:01 +0530

    Day1 code

---
+++ challenge/P1.py
@@ -0,0 +1,4 @@
+#Take user input and print formatted output using f-strings
+
+name = input("Enter your name: ")
+print(f"Hi {name}")
---
+++ challenge/P2.py
@@ -0,0 +1,17 @@
+#Convert between int, float, string, and boolean
+
+a = 1
+b = 1.5
+c = "ABC"
+d = True
+
+print(int(b))
+print(int(d))
+print(float(a))
+print(float(d))
+print(str(a))
+print(str(b))
+print(str(d))
+print(bool(a))
+print(bool(b))
+print(bool(c))
